6fe185398d5b8b49d30fbdbdf2a31fcf5caf2072
AlexFriedland <friedlandaj@gmail.com>
2018-06-01 11:16:21 +0200

    house attr

--- db/migrate/20180531214714_create_houses.rb
+++ db/migrate/20180531214714_create_houses.rb
@@ -1,6 +1,10 @@
 class CreateHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :houses do |t|
+      t.string :info
+      t.integer :value
+      t.integer :purchase_price
+      t.integer :market_value
 
       t.timestamps
     end
