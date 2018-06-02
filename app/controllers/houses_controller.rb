26a2ef7c3778dc69e12115038ed47f080adf2ecc
AlexFriedland <friedlandaj@gmail.com>
2018-06-02 15:28:03 +0200

    house controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,47 @@
 class HousesController < ApplicationController
+
+  def new
+    @house = House.new
+  end
+
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def create
+    @house = House.new(house_params)
+  end
+
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+
+    if @house.save
+      redirect_to @house
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    flash[:notice] = "House deleted."
+    redirect_to houses_path
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :market_value, :info, :purchase_price, :total_expenses, :total_income, :value)
+  end
+
+
 end
