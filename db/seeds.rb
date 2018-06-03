330c982b086bf5a91916dcac3c14bcb09a7a9502
AlexFriedland <friedlandaj@gmail.com>
2018-06-03 02:18:52 +0200

    some fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,15 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', '/Users/Alexander/desktop/dev/houser/lib/seeds/Untitled.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  binding.pry
+
   week = Week.new(closed?: true, date: "#{row['Rental Week']}")
+  
   if row['Rental'] != nil
     deposit = Deposit.new(amount: "#{row['Deposit']}", returned: true)
     rincome = Rincome.new(amount: "#{row['Rental']}")
     renter = Renter.new(name: "#{row['Renter']}")
     payment = Payment.new(paid: "#{row['Paid']}")
   end
-
 
   week.save;deposit.save;rincome.save;renter.save;payment.save
   @y.weeks << week
