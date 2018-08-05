d5e09519c0d4cf5cdbd0f6b2e805a1ede8305df0
AlexFriedland <friedlandaj@gmail.com>
2018-08-05 22:24:21 +0200

    user model, before controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_234455) do
+ActiveRecord::Schema.define(version: 2018_08_05_202033) do
 
   create_table "deposits", force: :cascade do |t|
     t.integer "week_id"
@@ -73,6 +73,12 @@ ActiveRecord::Schema.define(version: 2018_05_31_234455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["week_id"], name: "index_rincomes_on_week_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "weeks", force: :cascade do |t|
