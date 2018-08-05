5b3ceb5b7f99675f2fbf007b42a3a47ef98976e0
AlexFriedland <friedlandaj@gmail.com>
2018-08-05 22:55:42 +0200

    save user and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+
   end
 
   def create
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.save
+
+    redirect_to root_path
   end
 
   def something
