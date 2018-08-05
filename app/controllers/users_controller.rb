ebeb89c34adfd7e729143fb68821b6b05f7ace1d
AlexFriedland <friedlandaj@gmail.com>
2018-08-05 23:00:53 +0200

    validations email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,20 @@ class UsersController < ApplicationController
     @user.email = params[:user][:email]
     @user.save
 
+    if @user.save
+      redirect_to root_path
+    else
+      flash[:notice] = "invalid email"
+      render '/users/new'
+    end
+
+    # if params[:user][:email].present? && !User.exists?(:email => params[:user][:email])
+    #   @user.save
+    # else
+    #   #error logic
+    #   flash[:notice] = "your email is invalid"
+    # end
+
     redirect_to root_path
   end
 
