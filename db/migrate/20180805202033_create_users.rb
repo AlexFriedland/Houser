b8a2ec8c43f06cd9ecf49bc3a37ff536257fb2fa
AlexFriedland <friedlandaj@gmail.com>
2018-08-06 16:48:20 +0200

    add required FB properties to user model

--- db/migrate/20180805202033_create_users.rb
+++ db/migrate/20180805202033_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :email
       t.string :password_digest
+      t.string :name
+      t.string :image
+      t.string :uid
       t.timestamps
     end
   end
