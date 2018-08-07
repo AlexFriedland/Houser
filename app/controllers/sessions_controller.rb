5568df12689e5c4de4b3b4f1489cf6d3113ed965
AlexFriedland <friedlandaj@gmail.com>
2018-08-07 23:39:26 +0200

    omniauth controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,24 @@ class SessionsController < ApplicationController
     def create
       #raise cookies.inspect
 
-      if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        render 'sessions/new'
+      #facebook omniauth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
       end
+
+      session[:user_id] = @user.id
+
+      render 'welcome/home'
+
+      #regular auth
+      # if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
+      #   session[:user_id] = @user.id
+      #   redirect_to root_path
+      # else
+      #   render 'sessions/new'
+      # end
       #login
 
     end
@@ -22,7 +34,7 @@ class SessionsController < ApplicationController
       redirect_to '/login'
     end
 
-    
+
 
     private
 
