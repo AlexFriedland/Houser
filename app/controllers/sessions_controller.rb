744f7f1b47a678ed70ffa104756b73726326f2bf
AlexFriedland <friedlandaj@gmail.com>
2018-08-27 16:15:42 +0200

    omniauth setup pt 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,50 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :verify_user_is_authenticated, only: [:new, :create]
     def new
-
+      @user = User.new
     end
 
     def create
       #raise cookies.inspect
+      # @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      #   u.name = auth['info']['name']
+      #   u.email = auth['info']['email']
+      #   u.image = auth['info']['image']
+      # end
 
       #facebook omniauth
-      @user = User.find_or_create_by(uid: auth['uid']) do |u|
-        u.name = auth['info']['name']
-        u.email = auth['info']['email']
-        u.image = auth['info']['image']
-      end
 
-      session[:user_id] = @user.id
+      if auth_hash = request.env["omniauth.auth"]
+        #they logged in with OAuth
+        # raise auth_hash.inspect
 
-      render 'welcome/home'
+        oauth_email = request.env["omniauth.auth"]["info"]["email"]
+        if user = User.find_by(:email => oauth_email)
+          #if we know that user, log them in
 
-      #regular auth
-      # if @user = User.find_by(email: params[:email]) && @user.authenticate(params[:password])
-      #   session[:user_id] = @user.id
-      #   redirect_to root_path
-      # else
-      #   render 'sessions/new'
-      # end
-      #login
+          session[:user_id] = user.id
+          redirect_to root_path
+        else
+          user = User.new(email: oauth_email, password: SecureRandom.hex)
+          if user.save
+            session[:user_id] = user.id
+            redirect_to root_path
+          else
+
+          session[:user_id] = user.id
+
+          redirect_to root_path
+        end
+
+        #normal login
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+
+          redirect_to root_path
+        else
+          render 'sessions/new'
+        end
 
     end
 
