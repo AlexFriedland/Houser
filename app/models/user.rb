74ef133f27bfa6ed539b04406ce25ac2754d1df6
AlexFriedland <friedlandaj@gmail.com>
2018-09-05 23:43:44 +0200

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :password, presence: true
   :has_secure_password
   has_secure_password
 
