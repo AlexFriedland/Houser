3cd33901a3b76a27b40ac7dac9a29458c447354e
AlexFriedland <friedlandaj@gmail.com>
2018-10-02 16:04:27 -0400

    create and implement helper for user view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def all_years(user)
+    year_arr = []
+    user.houses.each do |h|
+      h.years.each do |y|
+        year_arr << y.year
+      end
+    end
+    year_arr.uniq!
+  end
+
 end
