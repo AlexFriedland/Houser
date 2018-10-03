fb3ee25d75910160897ce805a32c61010d3c0776
AlexFriedland <friedlandaj@gmail.com>
2018-10-03 18:29:43 -0400

    new year form

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -4,6 +4,30 @@ class YearsController < ApplicationController
     @year = Year.new
   end
 
+  def create
+    @house = House.new(house_params)
+    #if house saves,
+    if @house.save
+      @user = User.find(session[:user_id])
+
+      #logic to find + create new years
+      x = params[:house][:year_ids]
+      x[0] = "0"
+      x.each do |id|
+        y = Year.find_by_id(id)
+        if y != nil
+          @house.years << Year.create!(house_id: @house.id, year: y.year, total_income: 0)
+        end
+      end
+
+      @user.houses << @house
+      binding.pry
+      redirect_to houses_path
+    else
+      render 'house/new'
+    end
+  end
+
   def show
     @year = Year.find(params[:id])
   end
