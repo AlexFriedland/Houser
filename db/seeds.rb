0968928141785f852f71297a54a7fe747b2c621e
AlexFriedland <friedlandaj@gmail.com>
2018-10-03 15:56:48 -0400

    made first seed work, move on

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,107 +55,122 @@ end
 csv_text = nil
 csv = nil
 
-binding.pry
+
+# FIX THE REST FOR SEEDING FROM DB REFACTOR
+
+
+
+
 
 #------------------
-
-
-
-@h.years << Year.create!(house_id: @h.id, year: 2008, total_income: 0)
-@y = @h.years.last
-
-csv_text = File.read(Rails.root.join('lib', 'seeds', '/Users/Alexander/desktop/dev/houser/lib/seeds/Bayberry08.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-
-
-
-total_income = 0
-
-csv.each do |row|
-
-  if row['Rental'] != nil
-    @y.weeks << Week.create(closed?: true, date: "#{row['Rental Week']}")
-    week = @y.weeks.last
-
-    week.renters << Renter.new(week_id: week.id, name: row['Renter'])
-    week.week_renters << WeekRenter.new(week_id: week.id, renter_id: week.renters.last.id)
-
-    week.deposits << Deposit.new(week_id: week.id, amount: row['Deposit'].split(" ")[1].gsub(",", "").split(".")[0], returned: true)
-
-    week.rincomes << Rincome.new(week_id: week.id, amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0], percentPaid: 100)
-    week.payments << Payment.new(rincome_id: week.rincomes.last.id, payment_type: row['Paid'], amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0])
-    week.payments.last.rincomes << week.rincomes.last
-
-    total_income += week.rincomes.last.amount if week.rincomes.last.amount > 0
-
-    week.save
-  end
-end
-
-@y.total_income = total_income
-@h.save
-@y.save
-csv_text = nil
-csv = nil
-
+#
+#
+#
+# @h.years << Year.create!(house_id: @h.id, year: 2008, total_income: 0)
+# @y = @h.years.last
+#
+# csv_text = File.read(Rails.root.join('lib', 'seeds', '/Users/Alexander/desktop/dev/houser/lib/seeds/Bayberry08.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+#
+#
+#
+# total_income = 0
+#
+# csv.each do |row|
+#
+#   if row['Rental'] != nil
+#     @y.weeks << Week.create(closed?: true, date: "#{row['Rental Week']}")
+#     week = @y.weeks.last
+#
+#     week.renters << Renter.new(week_id: week.id, name: row['Renter'])
+#     week.week_renters << WeekRenter.new(week_id: week.id, renter_id: week.renters.last.id)
+#
+#     week.deposits << Deposit.new(week_id: week.id, amount: row['Deposit'].split(" ")[1].gsub(",", "").split(".")[0], returned: true)
+#
+#     week.rincomes << Rincome.new(week_id: week.id, amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0], percentPaid: 100)
+#     week.payments << Payment.new(rincome_id: week.rincomes.last.id, payment_type: row['Paid'], amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0])
+#     week.payments.last.rincomes << week.rincomes.last
+#
+#     total_income += week.rincomes.last.amount #if week.rincomes.last.amount > 0
+#
+#     week.save
+#   end
+# end
+#
+# @y.total_income = total_income
+# @h.save
+# @y.save
+# csv_text = nil
+# csv = nil
+#
 
 #------------------
 #2009
+#
+#
+# @h.years << Year.create!(house_id: @h.id, year: 2009, total_income: 0)
+# @y = @h.years.last
+#
+# csv_text = File.read(Rails.root.join('lib', 'seeds', '/Users/Alexander/desktop/dev/houser/lib/seeds/Bayberry09.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+#
+#
+#
+# total_income = 0
+#
+# csv.each do |row|
+#
+#   if row['Rental'] != nil
+#     @y.weeks << Week.create(closed?: true, date: "#{row['Rental Week']}")
+#     week = @y.weeks.last
+#
+#     week.misc_charges = row['Misc'].to_i if !row['Misc'] == nil
+#
+#     week.renters << Renter.new(week_id: week.id, name: row['Renter'])
+#     week.week_renters << WeekRenter.new(week_id: week.id, renter_id: week.renters.last.id)
+#
+#     if row['Deposit']
+#       week.deposits << Deposit.new(week_id: week.id, amount: row['Deposit'].split(" ")[1].gsub(",", "").split(".")[0], returned: true, notes: "#{row['Misc']}: #{row['Description']}")
+#     else
+#       week.deposits << Deposit.new(week_id: week.id, amount: 0, returned: true, notes: "#{row['Misc']}: #{row['Description']}")
+#     end
+#
+#     week.rincomes << Rincome.new(week_id: week.id, amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0], percentPaid: 100)
+#     week.payments << Payment.new(rincome_id: week.rincomes.last.id, payment_type: row['Paid'], amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0])
+#     week.payments.last.rincomes << week.rincomes.last
+#
+#
+#     total_income += week.rincomes.last.amount if week.rincomes.last.amount > 0
+#     total_income += week.misc_charges if week.misc_charges
+#
+#     week.save
+#   end
+# end
+#
+# @y.total_income = total_income
+# @h.save
+# @y.save
+# csv_text = nil
+# csv = nil
+#
+#
+#
+# puts "done"
+# puts "years: #{Year.count}"
+# puts "new weeks: #{Year.last.weeks.count}"
+# puts "total users: #{User.count}"
+#
+# puts "#{@y.total_income}"
 
 
-@h.years << Year.create!(house_id: @h.id, year: 2009, total_income: 0)
-@y = @h.years.last
-
-csv_text = File.read(Rails.root.join('lib', 'seeds', '/Users/Alexander/desktop/dev/houser/lib/seeds/Bayberry09.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
 
 
-total_income = 0
-
-csv.each do |row|
-
-  if row['Rental'] != nil
-    @y.weeks << Week.create(closed?: true, date: "#{row['Rental Week']}")
-    week = @y.weeks.last
-
-    week.misc_charges = row['Misc'].to_i if !row['Misc'] == nil
-
-    week.renters << Renter.new(week_id: week.id, name: row['Renter'])
-    week.week_renters << WeekRenter.new(week_id: week.id, renter_id: week.renters.last.id)
-
-    if row['Deposit']
-      week.deposits << Deposit.new(week_id: week.id, amount: row['Deposit'].split(" ")[1].gsub(",", "").split(".")[0], returned: true, notes: "#{row['Misc']}: #{row['Description']}")
-    else
-      week.deposits << Deposit.new(week_id: week.id, amount: 0, returned: true, notes: "#{row['Misc']}: #{row['Description']}")
-    end
-
-    week.rincomes << Rincome.new(week_id: week.id, amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0], percentPaid: 100)
-    week.payments << Payment.new(rincome_id: week.rincomes.last.id, payment_type: row['Paid'], amount: row['Rental'].split(" ")[1].gsub(",", "").split(".")[0])
-    week.payments.last.rincomes << week.rincomes.last
-
-
-    total_income += week.rincomes.last.amount if week.rincomes.last.amount > 0
-    total_income += week.misc_charges if week.misc_charges
-
-    week.save
-  end
-end
-
-@y.total_income = total_income
-@h.save
-@y.save
-csv_text = nil
-csv = nil
+#-----------------
 
 
 
-puts "done"
-puts "years: #{Year.count}"
-puts "new weeks: #{Year.last.weeks.count}"
-puts "total users: #{User.count}"
 
-puts "#{@y.total_income}"
 
 
 =begin
