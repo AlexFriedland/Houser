f514dbe6e1f53ab13d2dfed39872425b4594328a
AlexFriedland <friedlandaj@gmail.com>
2019-01-11 22:32:05 -0500

    load comments with $.get works

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -2,22 +2,36 @@
  $(document).ready(function(e){
    //also worked with click
      $("a.load_comments").on("click", function(e){
-       //fire ajax GET request when link clicked
-       $.ajax({
-         method: "GET",
-         url: this.href
-         //when done do XYZ with data:
-       }).done(function(server_response){
 
+       // PASS ONE - AJAX
+       // fire ajax GET request when link clicked
+
+       // $.ajax({
+       //   method: "GET",
+       //   url: this.href
+       //   //when done do XYZ with data:
+       // }).done(function(server_response){
+       //
+       //   $("div.comments").html(server_response)
+       //
+       // }).error(function(notNeeded){
+       //   alert('something broke')
+       // })
+
+       // SECOND PASS
+       // JQUERY API FOR REQUESTS (Jquery.get)
+       $.get(this.href).done(function(server_response){
          $("div.comments").html(server_response)
-
        })
+
        e.preventDefault();
      })
 
      //hide comments
      $("a.hide_comments").on("click", function(e){
-       $("div.comments").html("");
+       // $("div.comments").html("");
+       // OR
+       $("div.comments").empty()
        e.preventDefault();
      })
  })
