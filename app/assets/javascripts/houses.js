9e174343ffb3085869a5b188bd0a6761bbfb4443
AlexFriedland <friedlandaj@gmail.com>
2019-01-11 23:56:39 -0500

    load comments w client side works

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -2,6 +2,8 @@
  $(document).ready(function(e){
    //also worked with click
      $("a.load_comments").on("click", function(e){
+       $("div.comments").empty()
+
 
        // PASS ONE - AJAX
        // fire ajax GET request when link clicked
@@ -20,14 +22,40 @@
 
        // SECOND PASS
        // JQUERY API FOR REQUESTS (Jquery.get)
-       $.get(this.href).done(function(server_response){
-         $("div.comments").html(server_response)
+       // $.get(this.href).done(function(server_response){
+       //   $("div.comments").html(server_response)
+       // })
+
+       // THIRD PASS W JSON
+       // you'd also need to set the controller render to json (see comments_controller)
+       // $.get(this.href).done(function(json){
+       //   //iterate over each JSON comment and append new LI foreach
+       //   var $ol = $("div.comments")
+       //   $ol.append("<ol></ol>")
+       //   $ol = $("div.comments ol")
+       //   json.forEach(function(comment){
+       //     $ol.append("<li>" + comment.body + "</li>")
+       //   })
+       // })
+
+       // PASS THREE - server side
+       // previously browser was handling
+
+       $.ajax({
+         url: this.href,
+         // script in index.js.erb
+         dataType: 'script'
        })
+
+
 
        e.preventDefault();
      })
 
-     //hide comments
+
+
+     // HIDE COMMENTS
+
      $("a.hide_comments").on("click", function(e){
        // $("div.comments").html("");
        // OR
