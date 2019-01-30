7d47579f9bbdd2c1b20b1bbe117b03d3a3bc1dff
AlexFriedland <friedlandaj@gmail.com>
2019-01-30 02:19:35 -0500

    sleep, changing comments index back to just ol for rendering but first need make video

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,8 +1,15 @@
 $(document).ready(function(e){
 
-  alert("loaded")
   var test = "TEST"
-  //
+
+  $("a.load_comments2").on("click", function(e){
+    e.preventDefault()
+    $.get(this.href).done(function(json){
+      console.log(json)
+    })
+  })
+
+
   // $("a.hide_comments").on("click", function(e){
   //   $("div.comments").empty()
   //   e.preventDefault();
