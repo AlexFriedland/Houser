7c91cd7b6d75e7986b43dd805cb782274810ebb7
AlexFriedland <friedlandaj@gmail.com>
2019-01-30 02:12:59 -0500

    added new link for without remote true

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,18 +1,19 @@
 $(document).ready(function(e){
 
-  alert("test")
-
-  $("a.hide_comments").on("click", function(e){
-    $("div.comments").empty()
-    e.preventDefault();
-  })
-
-  $("a.load_comments").on("click", function(e){
-    
-    $("div.comments").empty()
-    e.preventDefault()
-    $.get(this.href).done(function(server_response){
-      $("div.comments").html(server_response)
-    })
-  })
+  alert("loaded")
+  var test = "TEST"
+  //
+  // $("a.hide_comments").on("click", function(e){
+  //   $("div.comments").empty()
+  //   e.preventDefault();
+  // })
+  //
+  // $("a.load_comments").on("click", function(e){
+  //
+  //   $("div.comments").empty()
+  //   e.preventDefault()
+  //   $.get(this.href).done(function(server_response){
+  //     $("div.comments").html(server_response)
+  //   })
+  // })
 })
