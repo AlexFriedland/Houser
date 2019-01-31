9f1b66e8b7fc69d9ab027c2919d0f575e94b753a
AlexFriedland <friedlandaj@gmail.com>
2019-01-30 20:51:46 -0500

    load comments w JSON

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,26 +1,40 @@
 $(document).ready(function(e){
 
-  var test = "TEST"
-
   $("a.load_comments2").on("click", function(e){
     e.preventDefault()
-    $.get(this.href).done(function(json){
-      console.log(json)
-    })
+    // $.get(this.href).done(function(json){
+    //   console.log(json)
+    //   debugger
+    //   x = $.parseJSON(json);
+    //   console.log(x)
+    // })
+    $.ajax({
+      dataType: "json",
+      url: this.href,
+    }).done(function(json){
+
+      var $ol = $("div.comments")
+      $ol.append("<ol></ol>")
+      $ol = $("div.comments ol")
+      json.forEach(function(comment){
+        $ol.append("<li>" + comment.body + "</li>")
+      })
+    });
+    
   })
-
-
-  // $("a.hide_comments").on("click", function(e){
-  //   $("div.comments").empty()
-  //   e.preventDefault();
-  // })
-  //
-  // $("a.load_comments").on("click", function(e){
-  //
-  //   $("div.comments").empty()
-  //   e.preventDefault()
-  //   $.get(this.href).done(function(server_response){
-  //     $("div.comments").html(server_response)
-  //   })
-  // })
+//
+//
+//   $("a.hide_comments").on("click", function(e){
+//     $("div.comments").empty()
+//     e.preventDefault();
+//   })
+//
+//   $("a.load_comments").on("click", function(e){
+//
+//     $("div.comments").empty()
+//     e.preventDefault()
+//     $.get(this.href).done(function(server_response){
+//       $("div.comments").html(server_response)
+//     })
+//   })
 })
