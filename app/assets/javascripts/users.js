4e09b4eec376702cbc2c82a17d516eb3b4d7a3be
AlexFriedland <friedlandaj@gmail.com>
2019-02-01 02:25:56 -0500

    a bunch of useless additions to the user html files since actually all i'm doing is rendering the JS prototype from JSON

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,17 @@
 // get JSON from user, instantiate new class and render on page.
 
 function User(x){
-  
+  this.id = x.id;
+  this.name = x.name;
+  this.email = x.email;
 }
+
+User.prototype.renderUser = function(){
+  debugger
+}
+
+$(document).ready(function(e){
+
+
+  
+})
